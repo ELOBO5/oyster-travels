652b2775d22fd3d515e02d1be85a44ede56a434d
Tobi Kuyoro <38296981+tobikuyo@users.noreply.github.com>
2021-10-12 21:43:27 +0100

    Revert "Server with auth"

--- server/oystercard/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Comment, Experience, ExperienceReaction, Flight, FlightReaction, Trip
-
-admin.site.register(Trip)
-admin.site.register(Flight)
-admin.site.register(Experience)
-admin.site.register(FlightReaction)
-admin.site.register(ExperienceReaction)
-admin.site.register(Comment)
