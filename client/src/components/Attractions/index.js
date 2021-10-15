9f61469516a652a941bb6afd4d31d68397cf4915
Tobi Kuyoro <tobikuyo@gmail.com>
2021-10-15 06:37:23 +0100

    Changes across client

--- client/src/components/Attractions/index.js
+++ client/src/components/Attractions/index.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPlacesData } from '../../apis/places';
 import { addAttractions } from '../../redux/actions';
+import AttractionDetail from '../AttractionDetail';
 import './styles.css';
 
 const Attractions = () => {
     const dispatch = useDispatch();
-    const { bounds, attractions, tripId, destinationCity } = useSelector(state => state);
+    const { bounds, attractions, destinationCity } = useSelector(state => state);
 
     useEffect(() => {
         const setAttractionData = async () => {
@@ -20,61 +20,12 @@ const Attractions = () => {
         setAttractionData();
     }, [bounds, dispatch]);
 
-    const addAttractionToTrip = async attraction => {
-        const attractionInfo = {
-            trip: tripId,
-            category: 'attraction',
-            name: attraction.name,
-            rating: attraction.rating,
-            review_count: attraction.num_reviews,
-            image: attraction.photo.images.large.url,
-            address: attraction.address,
-            website_link: attraction.website,
-            tripadvisor_link: attraction.web_url,
-            price: attraction.price,
-            ranking: attraction.ranking
-        };
-
-        try {
-            await axios.post('http://localhost:8000/api/experiences/', attractionInfo);
-        } catch (error) {
-            console.error('ADD ATTRACTION ', error);
-        }
-    };
-
     return (
         <div className="experiences">
             <h1>Attractions in {destinationCity}</h1>
             <div className="experiences__container">
                 {attractions?.map(attraction => (
-                    <div className="experiences__card" key={attraction.location_id}>
-                        <img src={attraction.photo.images.large.url} alt={attraction.name} />
-                        <div className="experiences__card-content">
-                            <h2>{attraction.name}</h2>
-                            <p className="experiences__ranking">{attraction.ranking}</p>
-                            <p>
-                                <span>Rating: </span> {Number(attraction.rating)}
-                            </p>
-                            <p>
-                                <span>Reviews: </span> {attraction.num_reviews}
-                            </p>
-                            <p>
-                                <span>Address: </span> {attraction.address}
-                            </p>
-                            <div className="experiences__websites">
-                                {attraction.website && (
-                                    <a href={attraction.website}>Website</a>
-                                )}
-
-                                {attraction.web_url && (
-                                    <a href={attraction.web_url}>Tripadvisor link</a>
-                                )}
-                            </div>
-                            <button onClick={() => addAttractionToTrip(attraction)}>
-                                Add to Trip
-                            </button>
-                        </div>
-                    </div>
+                    <AttractionDetail attraction={attraction} />
                 ))}
             </div>
         </div>
--- client/src/pages/Accommodation/index.js
+++ client/src/pages/Accommodation/index.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPlacesData } from '../../apis/places';
 import { addHotels } from '../../redux/actions';
+import HotelDetail from '../../components/HotelDetail';
 import './style.css';
 
 const Accommodations = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const { bounds, hotels, tripId, destinationCity } = useSelector(state => state);
+    const { bounds, hotels, destinationCity } = useSelector(state => state);
 
     useEffect(() => {
         const setAccommodationData = async () => {
@@ -22,45 +22,12 @@ const Accommodations = () => {
         setAccommodationData();
     }, [bounds, dispatch]);
 
-    const addHotelToTrip = async ({ name, rating, num_reviews, photo, price, ranking }) => {
-        const hotel = {
-            trip: tripId,
-            category: 'hotel',
-            name,
-            rating,
-            price,
-            ranking,
-            review_count: num_reviews,
-            image: photo.images.large.url
-        };
-
-        try {
-            await axios.post('http://localhost:8000/api/experiences/', hotel);
-        } catch (error) {
-            console.error('ADD HOTEL ', error);
-        }
-    };
-
     return (
         <div className="hotels">
             <h1>Hotels in {destinationCity}</h1>
             <div className="hotels__container">
-                {hotels?.map(hotel => (
-                    <div key={hotel.location_id} className="hotels__card">
-                        <img src={hotel.photo.images.large.url} alt={hotel.name} />
-                        <h2>{hotel.name}</h2>
-                        <p className="hotels__ranking"> {hotel.ranking}</p>
-                        <p>
-                            <span>Rating:</span> {Number(hotel.rating)}
-                        </p>
-                        <p>
-                            <span>Reviews:</span> {hotel.num_reviews}
-                        </p>
-                        <p>
-                            <span>Price Range:</span> {hotel.price}
-                        </p>
-                        <button onClick={() => addHotelToTrip(hotel)}>Add to Trip</button>
-                    </div>
+                {hotels.map(hotel => (
+                    <HotelDetail hotel={hotel} key={hotel.location_id} />
                 ))}
             </div>
             <button onClick={() => history.push('/experiences')}>View Experiences</button>
