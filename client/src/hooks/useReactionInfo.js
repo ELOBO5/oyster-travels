ac1a57a3e095d7e02fe53790d344bcac4dc93aed
“ELOBO5” <elicialobo@hotmail.com>
2021-10-15 09:10:19 +0100

    update link

--- client/src/hooks/useReactionInfo.js
+++ client/src/hooks/useReactionInfo.js
@@ -14,11 +14,20 @@ const useReactionInfo = (experience, userReactions) => {
     useEffect(() => {
         const getReactionInfo = async () => {
             try {
-                const { data } = await axios.get(`http://localhost:8000/api/experiences/${experience.id}/`);
+                const { data } = await axios.get(
+                    `https://oystertravel.herokuapp.com/api/experiences/${experience.id}/`
+                );
 
                 const currentUserId = Number(localStorage.getItem('user_id'));
-                const userHasLiked = data.reactions.find(reaction => reaction.like && reaction.user === currentUserId);
-                const userHasDisliked = data.reactions.find(reaction => reaction.dislike && reaction.user === currentUserId);
+
+                const userHasLiked = data.reactions.find(
+                    reaction => reaction.like && reaction.user === currentUserId
+                );
+
+                const userHasDisliked = data.reactions.find(
+                    reaction => reaction.dislike && reaction.user === currentUserId
+                );
+
                 const likes = data.reactions.filter(reaction => reaction.like);
                 const dislikes = data.reactions.filter(reaction => reaction.dislike);
 
@@ -32,7 +41,7 @@ const useReactionInfo = (experience, userReactions) => {
         };
 
         getReactionInfo();
-    }, [experience.id, rerender, userHasLiked, userHasDisliked]);
+    }, [experience.id, rerender]);
 
     const handleLike = () => {
         likeExperience();
